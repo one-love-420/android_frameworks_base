9d88338dea126d05d677b42d2d5ef7b270eaf4f5
Jubakuba <Jubakuba@gmail.com>
2014-10-17 00:39:40 -0600

    Framework: StatusBar Ticker (1/2)
    Change-Id: I6135297792c7b42172e08e14acd37b252cb00102
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2075,11 +2075,19 @@ public final class Settings {
          */
         public static final String BUTTON_BACKLIGHT_TIMEOUT = "button_backlight_timeout";
 
-        /** Whether to allow one finger quick settings expansion on the right side of the statusbar.
+        /** 
+         * Whether to allow one finger quick settings expansion on the right side of the statusbar.
          *
          * @hide
          */
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
+        /**
+         * Whether to enable ticker animation in statusbar
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
 
         /**
          * Control whether the process CPU usage meter should be shown.
