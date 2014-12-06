5f7540c4ef06fdd7d7c291c7a116d8484ea8e2e3
Byunghun Jeon <bjeon@codeaurora.org>
2014-12-05 18:44:11 -0800

    WindowManager: Upper level changes to expose blur effect
    Upper level changes to expose blur-behind and blur-mask effect
    
    Change-Id: I6d37b43888c8c5e028974bd714596d8178cb5114
    
    WindowManager: Adding template for BlurLayer file
    
    Cyanogen changes:
      * Use config.xml instead of settings
      * Don't break stuff if not supported in SF
      * Incremental fade for insecure lockscreen
      * Disabled extra effects for now
    
    Change-Id: Icefe452f7a015656661e8543849c9b88889dbb40

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2583,6 +2583,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         return mScreenOnComingFromTouch;
     }
 
+    void setBlur(float b){
+        mStatusBarWindowManager.setBlur(b);
+    }
+
     public boolean isFalsingThresholdNeeded() {
         boolean onKeyguard = getBarState() == StatusBarState.KEYGUARD;
         boolean isMethodInsecure = mUnlockMethodCache.isMethodInsecure();
@@ -3557,7 +3561,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void addStatusBarWindow() {
         makeStatusBarView();
         mStatusBarWindow.addContent(mStatusBarWindowContent);
-        mStatusBarWindowManager = new StatusBarWindowManager(mContext);
+        mStatusBarWindowManager = new StatusBarWindowManager(mContext, this);
         mStatusBarWindowManager.add(mStatusBarWindow, getStatusBarHeight());
     }
 
@@ -4321,6 +4325,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         updateKeyguardState(staying, false /* fromShadeLocked */);
         return staying;
+    }
+
+    boolean isSecure() {
+        return mStatusBarKeyguardViewManager != null && mStatusBarKeyguardViewManager.isSecure();
     }
 
     public long calculateGoingToFullShadeDelay() {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -205,6 +205,7 @@ public class PhoneStatusBarView extends PanelBar {
         super.panelExpansionChanged(panel, frac, expanded);
         mScrimController.setPanelExpansion(frac);
         mBar.updateCarrierLabelVisibility(false);
+        mBar.setBlur(frac);
     }
 
     @Override
