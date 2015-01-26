9a32306032bcd9a3f0d64e2ce5797b236557ebd2
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-26 15:45:59 +0000

    SettingsProvider: fix upgrade path
    Change-Id: I5d083203e658e99d150db91f1a4692ef45df2c63

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -1905,7 +1905,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             upgradeVersion = 117;
         }
 
-       if (upgradeVersion == 118) {
+       if (upgradeVersion == 117) {
             db.beginTransaction();
             SQLiteStatement stmt = null;
             try {
